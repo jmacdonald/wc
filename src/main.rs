87369d9a8e37deb5e374d7e9292c65fe1bc5cdf2
Jordan MacDonald <jordan@wastedintelligence.com>
2017-11-12 13:02:12 -0500

    Initial basic implementation

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,21 @@
+use std::env;
+use std::fs::File;
+use std::io::Read;
+use std::path::Path;
+
 fn main() {
+    let args = env::args();
+    let path_arg = args.skip(1).nth(0).unwrap();
+    let path = Path::new(&path_arg);
+
+    let mut file = File::open(&path).unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+
+    let mut word_count = 0;
+    for line in contents.lines() {
+        word_count += line.split_whitespace().count();
+    }
+
+    println!("{} words found", word_count);
 }
